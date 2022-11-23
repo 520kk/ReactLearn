46fd2e4e8aa86ef15419f14de7103e9b6f568ec3
Choiseokjae <829520k@gmail.com>
2022-11-23 17:39:05 +0900

    11.23

--- learn/src/practice_use/Accommodate.jsx
+++ learn/src/practice_use/Accommodate.jsx
@@ -10,7 +10,7 @@ function Accommodate(props) {
     // 카운트값이 바뀔때마다 호출됨.
     useEffect(() => {
         console.log("======================");
-        console.log("useEffect() is called.");
+        console.log("(의존성 배열이 있는형태) useEffect() is called.");
         console.log(`isFull: ${isFull}`);
     });
 
@@ -18,19 +18,19 @@ function Accommodate(props) {
     //Component가 update될때 마다 호출됨.
     useEffect(() => {
         setIsFull(count >= MAX_CAPACITY);
-        console.log(`Current count value: ${count}`);
+        console.log(`(의존성배열이 없는형태) Current count value: ${count}`);
     }, [count]);
 
     return (
         <div style={{ padding: 16 }}>
-            <p>{`총 ${count}명 수용했습니다.`}</p>
+            <p>{`총 ${count}명 입장 했습니다.`}</p>
 
             <button onClick={increaseCount} disabled={isFull}>
                 입장
             </button>
             <button onClick={decreaseCount}>퇴장</button>
 
-            {isFull && <p style={{ color: "red" }}>정원이 가득찼습니다.</p>}
+            {isFull && <p style={{ color: "red" }}>인원이 가득찼습니다.</p>}
         </div>
     );
 }
